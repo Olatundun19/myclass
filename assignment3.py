6fabb6534748dfe131d7956bf69e5dd29fd507b5
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2022-09-19 10:56:34 +0100

    update

--- assignment3.py
+++ assignment3.py
@@ -1,10 +1,10 @@
 print('This programme converts the unit of values')
-print('1.Km to m, 2.m to cm, 3.cm to mm, 4.inches to m, 5.Feet to inches')
+print('1. km to m 2. m to cm 3. cm to mm 4. inches to m 5. Feet to inches')
 opt=input('Enter Option Below:')
-x=float(input('Enter value for convertion:'))
+x=float(input('Enter the value for conversion:'))
 if opt=='1':
     i=x*1000
-    print(i)
+    print(str(x) + 'km = ' + str(i) + 'm')
 elif opt=='2':
     i=x*100
     print(i)
---
+++ errorhandling.py
@@ -0,0 +1,22 @@
+try:
+    age = int(input('Enter your age: '))
+    print('Your age is ' + str(age))
+
+except:
+    print('You have made a wrong entry, Pls make sure you enter your age!!!')
+
+else:
+    print('No error!!!')
+
+finally:
+    print('Program executed successfully!!!')
+
+
+
+
+
+
+
+
+
+
