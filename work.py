48581b27a4fd9c0e31a117e393430c3abcfdaaa6
Olatundun19 <olayemiolatee@gmail.com>
2022-09-20 13:05:43 +0100

    new

---
+++ work.py
@@ -0,0 +1,6 @@
+a= float(input('Enter length of the rectangle:'))
+b=float(input('Enter breadth of the rectangle'))
+c= a*b
+area= 'The area of rectangle= {} * {} = {} '
+d= area.format(a,b,c)
+print(d)
