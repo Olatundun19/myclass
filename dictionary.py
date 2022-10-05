9afe8f8d2a558967fd287a62ce787f3a1a7212f7
Olatundun19 <113788919+Olatundun19@users.noreply.github.com>
2022-10-05 01:32:18 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,23 @@
 words = {
         'pencil':'Writing material',
         'chair':'Material to sit on',
-        'table':'Material to write on'
+        'table':'Material to write on',
+        'cow': 'A female domestic bovine animal, regardless of sex or age',
+        'sky': 'The region of the atmosphere and outer space seen from the earth',
+        'fan': 'An apparatus with rotating blades that creates a current of air for cooling or ventilation ',
+        'belt': 'A strip of leather or other material worn, typically round the waist, to support or hold in clothes or to carry weapons',
+        'cord': 'Thin,flexible string or rope made from several twisted strands',
+        'card': 'A piece of thick,siff paper or thin pasteboard,in particular one used for writing or printing on',
+        'bulb': 'An expanded part of a glass tube such as that forming the reservoir of a thermometer',
+        'pot': 'A rounded or cylindrical, typically of metal, used for cooking',
+        'spoon': 'An instrument consisting of a small round shallow bowl, used for eating,stirring and serving food',
+        'phone': 'A telephone',
+        'rain': 'The condensed moisture of the atmosphere, falling visibly in seperate drops',
+        'gown': 'A long elegant dress worn on formal occasions',
+        'book': 'A written or printed work consisting of pages glued or sewn together along one sideand bound in covers',
+        'water': 'A colourless,transparent,odourless liquid that forms the seas,lakes,rivers, and rain and is the basis of fluids of living organisms',
+        'plate': 'A flat dish, typically circular and made of china from which food is eaten or served',
+        'broom': 'A long-handled brush,used for sweeping'
 
 }
 print('Dictinary!!!')
