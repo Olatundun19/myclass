832a0759941b0a747f8967038b1518f5cc569323
Olatundun19 <113788919+Olatundun19@users.noreply.github.com>
2022-10-06 05:13:27 -0700

    Create meand.py

---
+++ meand.py
@@ -0,0 +1,17 @@
+n=int(input('ENTER THE NUMBER OF COURSES OFFERED: '))
+courses = []
+i=1
+j=0
+while i<=n:
+    x=int(input('ENTER SCORE:'))
+    if x<0 or x>100:
+        print('ERROR!!!INVALID SCORE')
+    else:
+        courses.append(x)
+        i=i+1
+        j=j+x
+k=j/n
+p=len(courses)-1
+i=0
+while i<=p:
+    x = abs(courses[i] - k)
